1dce892d3e1a242b87c14f7bfbed96a256fb2944
BottyIvan <droidbotty@gmail.com>
2025-09-04 23:06:15 +0200

    Add cant_disable attribute to ExtensionModel for better extension management

--- cloud/ivanbotty/Launcher/models/extension_model.py
+++ cloud/ivanbotty/Launcher/models/extension_model.py
@@ -11,6 +11,7 @@ class ExtensionModel(GObject.GObject):
         name (str): Extension name.
         description (str): Extension description.
         enabled (bool): Whether the extension is enabled.
+        cant_disable (bool): Whether the extension can be disabled.
         version (str): Extension version.
         author (str): Extension author.
         service: Associated service identifier.
@@ -20,10 +21,11 @@ class ExtensionModel(GObject.GObject):
     name = GObject.Property(type=str)
     description = GObject.Property(type=str, default=None)
     enabled = GObject.Property(type=bool, default=True)
+    cant_disable = GObject.Property(type=bool, default=False)
     version = GObject.Property(type=str, default=None)
     author = GObject.Property(type=str, default=None)
 
-    def __init__(self, name, description=None, enabled=True, service=None, handler=None, version=None, author=None):
+    def __init__(self, name, description=None, enabled=True, cant_disable=False, service=None, handler=None, version=None, author=None):
         """
         Initialize ExtensionModel.
 
@@ -31,6 +33,7 @@ class ExtensionModel(GObject.GObject):
             name (str): Extension name.
             description (str, optional): Extension description.
             enabled (bool, optional): Initial enabled state.
+            cant_disable (bool, optional): If True, the extension cannot be disabled.
             service (optional): Service identifier.
             handler (optional): Extension handler.
             version (str, optional): Extension version.
@@ -49,6 +52,6 @@ class ExtensionModel(GObject.GObject):
         else:
             self.enabled = enabled
             db.set_extension_enabled(service, enabled)
-
+        self.cant_disable = cant_disable
         self.version = version
         self.author = author
