5cdb61b710a5a698e66fd4766c9785c86168d188
BottyIvan <droidbotty@gmail.com>
2025-09-04 22:05:50 +0200

    Refactor AppHandler to improve application loading and view handling

--- cloud/ivanbotty/Launcher/handlers/applications_handler.py
+++ cloud/ivanbotty/Launcher/handlers/applications_handler.py
@@ -1,19 +1,19 @@
 from gi.repository import Gtk
 import cloud.ivanbotty.Launcher.handlers.base_input_handler as bih
+from cloud.ivanbotty.Launcher.widget import row as row_widget
 
 class AppHandler(bih.BaseInputHandler):
     def can_handle(self, text):
         # Anything that doesn't fit other handlers and doesn't start with >, ask, or http
         return True
 
-    def handle(self, text, services, listbox):
-        apps_service = services.get("app")
+    def handle(self, text, services, view):
+        apps_service = services.get("application")
         if apps_service:
+            listbox = Gtk.ListBox()
+            listbox.set_selection_mode(Gtk.SelectionMode.SINGLE)
+            listbox.add_css_class("boxed-list-separate")
             apps_service.load_applications()
             apps = apps_service.filter_applications(text)
-            listbox.remove_all()
-            for app in apps:
-                row = Gtk.Label(label=app.name)  # You can replace with Row(app) if you want
-                listbox.insert(row, 0)
-            if apps:
-                apps[0].launch()
+            listbox.bind_model(apps, lambda app: row_widget.Row(app))
+            view.set_child(listbox)
