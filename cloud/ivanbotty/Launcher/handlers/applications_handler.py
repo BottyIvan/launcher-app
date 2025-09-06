d131335fde7911e75942881fa47c332dcdb9d4d5
BottyIvan <droidbotty@gmail.com>
2025-09-06 18:22:27 +0200

    Refactor AppHandler to simplify handle method by removing unused view and listbox logic

--- cloud/ivanbotty/Launcher/handlers/applications_handler.py
+++ cloud/ivanbotty/Launcher/handlers/applications_handler.py
@@ -1,23 +1,13 @@
-from gi.repository import Gtk
 import cloud.ivanbotty.Launcher.handlers.base_input_handler as bih
-from cloud.ivanbotty.Launcher.widget import row as row_widget
 
 class AppHandler(bih.BaseInputHandler):
     def can_handle(self, text):
         # Anything that doesn't fit other handlers and doesn't start with >, ask, or http
         return True
 
-    def handle(self, text, services, view):
+    def handle(self, text, services):
         apps_service = services.get("application")
         if apps_service:
-            listbox = Gtk.ListBox()
-            listbox.set_selection_mode(Gtk.SelectionMode.SINGLE)
-            listbox.add_css_class("boxed-list-separate")
             apps_service.load_applications()
-            apps = apps_service.filter_applications(text)
-            listbox.bind_model(apps, lambda app: (
-                row := row_widget.Row(app),
-                setattr(row, "application", app),
-                row
-            ))
-            view.set_child(listbox)
+            return apps_service.filter_applications(text)
+        return None
