c604c2d7dc7017427f877ecf7617205fbee5de5b
BottyIvan <droidbotty@gmail.com>
2025-09-08 21:17:04 +0200

    Refactor KeyboardController to remove unused imports and streamline code

--- cloud/ivanbotty/Launcher/controller/keyboard_controller.py
+++ cloud/ivanbotty/Launcher/controller/keyboard_controller.py
@@ -1,6 +1,6 @@
-import gi,subprocess
+import gi
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk, Gdk, GObject
+from gi.repository import Gtk, Gdk
 
 class KeyboardController(Gtk.EventControllerKey):
     def __init__(self, app):
